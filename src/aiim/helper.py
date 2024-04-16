2034459c467e2af638423e2778e3087c75554af3
Johannes Melsbach <75705301+jmelsbach@users.noreply.github.com>
2024-04-16 22:17:17 +0200

    add interactive plot

--- src/aiim/helper.py
+++ src/aiim/helper.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import ipywidgets as widgets
 
 
 def plot_function(
@@ -11,7 +12,7 @@ def plot_function(
 ):
     """
     Plots the function f in the range [min, max] with num_points.
-    
+
     Args:
     f: function
     x: float
@@ -19,10 +20,16 @@ def plot_function(
     max: float
     num_points: int
     """
-    
+
     _x = np.linspace(min, max, num_points)
     y = f(_x)
     plt.plot(_x, y)
     plt.plot(x, f(x), "ro")
     plt.legend([f"{f.__name__}({x:.2f})={f(x):.2f}"])
     plt.show()
+
+
+def interactive_plot(fn, min=-8, max=8):
+    a = widgets.FloatSlider(description='Input Value', min=min, max=max)
+    out = widgets.interactive_output(plot_function, {'f': widgets.fixed(fn), 'x': a})
+    return widgets.VBox([a, out])
